604df038868506d1bd093e6bd1d33a126cc4d681
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 14:42:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 
 def batch_badge_creator(array)
   batch_badge = []
-  array.each {|name| batch_badge.push("Hello my name is #{name}.")}
+  array.each {|name| batch_badge.push("Hello, my name is #{name}.")}
   return batch_badge
 end 
