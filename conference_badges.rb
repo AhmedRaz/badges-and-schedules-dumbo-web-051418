359313172142a897c1d5d07d9205475925166757
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 15:31:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(array)
   return assigned_rooms
 end 
 
-def printer(attendees)
+def printer(array)
   batch_badge_print = batch_badge_creator(array)
   room_assigned = assign_rooms(array)
   batch_badge_print.each { |item| puts item}
